abfce9c5e5a6bc51318e18ff1c35583707f5eec8
danial <danialdark12098@gmail.com>
2021-12-15 12:17:40 +0330

    adding admin user controller

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -15,9 +15,87 @@ class AdminUserController extends Controller
      */
     public function index()
     {
-        $users = User::get();
-        return view('admin.users.index',compact('users'));
+        $users = User::all();
+        return view("admin.users.index", compact("users"));
     }
+
+    // safe haye har role ro miare 
+    public function normal()
+    {
+        $users = User::all();
+        return view("admin.users.normal", compact("users"));
+    }
+
+
+    public function writer()
+    {
+        $users = User::all();
+        // dd($users[0]->roles()->exists("admin"));
+        return view("admin.users.writer", compact("users"));
+    }
+
+
+    public function admin()
+    {
+        $users = User::all();
+        // dd($users[0]->roles()->exists("admin"));
+        return view("admin.users.admin", compact("users"));
+    }
+    // safe haye har role ro miare 
+
+
+
+
+    // give admin access
+    public function promotetoadmin($id)
+    {
+        $user = User::find($id);
+        $role = Role::where("name", "admin")->first();
+        $user->roles()->attach($role->id);
+        return redirect()->back()->with("success", " دسترسی مدریرت به کاربر داده شد");
+    }
+
+    // remove admin access
+    public function demoteadmin($id)
+    {
+        $user = User::find($id);
+        $role = Role::where("name", "admin")->first();
+        $user->roles()->detach($role->id);
+        return redirect()->back()->with("success", " دسترسی مدیریت از کاربر گرفته شد");
+    }
+
+
+    // give writer access
+    public function promotetowriter($id)
+    {
+        $user = User::find($id);
+        $role = Role::where("name", "writer")->first();
+        $user->roles()->attach($role->id);
+        return redirect()->back()->with("success", " دسترسی نویسندگی به کاربر داده شد");
+    }
+
+
+    // remove writer access
+    public function demotewriter($id)
+    {
+        $user = User::find($id);
+        $role = Role::where("name", "writer")->first();
+        $user->roles()->detach($role->id);
+        return redirect()->back()->with("success", " دسترسی نویسندگی از کاربر گرفته شد");
+    }
+
+    // remove all access
+    public function clearroles($id)
+    {
+        $user = User::find($id);
+        $role = Role::where("name", "user")->first();
+        $user->roles()->sync($role->id);
+        return redirect()->back()->with("success", "تمامی دسترسی ها از کاربر گرفته شد");
+    }
+
+
+
+
 
     /**
      * Show the form for creating a new resource.
@@ -37,7 +115,14 @@ class AdminUserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->gender = $request->gender;
+        $user->Job = $request->Job;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return redirect()->back()->with("success", ".کاربر شما با موفقیت اضافه شد");
     }
 
     /**
@@ -82,6 +167,8 @@ class AdminUserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        $user->delete();
+        return redirect()->back()->with("success", "کاربر شما با موفقیت حذف شد");
     }
 }
