96dcd134f827d7faf9cfacd14da6b7b7d944d30d
danial <danialdark12098@gmail.com>
2021-12-15 12:18:03 +0330

    nothing important

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class AdminUserController extends Controller
 {
