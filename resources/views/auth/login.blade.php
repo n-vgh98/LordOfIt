c78712bca4d421735325931dd549abf369e859c0
danial <danialdark12098@gmail.com>
2021-12-15 12:00:06 +0330

    fixing miss vaghefi form problem

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,16 +47,17 @@
                     </span>
                 </div>
 
-                <form class="login100-form validate-form">
+                <form class="login100-form validate-form" action="{{ route('login') }}" method="POST">
+                    @csrf
                     <div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
                         <span class="label-input100">Username</span>
-                        <input class="input100" type="text" name="username" placeholder="Enter username">
+                        <input class="input100" type="text" name="email" placeholder="Enter username">
                         <span class="focus-input100"></span>
                     </div>
 
                     <div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
                         <span class="label-input100">Password</span>
-                        <input class="input100" type="password" name="pass" placeholder="Enter password">
+                        <input class="input100" type="password" name="password" placeholder="Enter password">
                         <span class="focus-input100"></span>
                     </div>
 
