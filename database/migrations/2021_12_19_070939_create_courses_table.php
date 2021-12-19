07f90e4737b32608e09ea0675e7962221e79d60f
danial <danialdark12098@gmail.com>
2021-12-19 11:15:26 +0330

    adding favorite and courses migration controller and model

---
+++ database/migrations/2021_12_19_070939_create_courses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("type");
+            $table->string("level");
+            $table->string("section");
+            $table->text("pre_need")->default("ندارد");
+            $table->text("price")->comment("dalil inke text hast ine ke mikhan benevisam dakhelesh va momkene ziad beshe");
+            $table->string("lang");
+            $table->text("description");
+            $table->text("topic_list");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
