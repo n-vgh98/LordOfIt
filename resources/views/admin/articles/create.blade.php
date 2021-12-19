b1aaf5480f23fdd090b3cd521d20d02992865d74
danial <danialdark12098@gmail.com>
2021-12-19 15:09:37 +0330

    adding ck_editor and images to article create page

--- resources/views/admin/articles/create.blade.php
+++ resources/views/admin/articles/create.blade.php
@@ -50,10 +50,79 @@
         </div><br>
         <div>
             <div>
+                {!! Form::label('text_2', 'متن شماره 2:') !!}
+                <? echo htmlspecialchars( $contentFromDB ); ?>
+                {!! Form::textarea('text_2', null, ['class' => 'form-control', 'id' => 'description2']) !!}
+            </div><br>
+        </div>
+        <div>
+            <div>
+                {!! Form::label('text_3', 'متن شماره 3:') !!}
+                <? echo htmlspecialchars( $contentFromDB ); ?>
+                {!! Form::textarea('text_3', null, ['class' => 'form-control', 'id' => 'description3']) !!}
+            </div><br>
+        </div>
+        <div>
+            <div>
+                {!! Form::label('text_4', 'متن شماره 4:') !!}
+                <? echo htmlspecialchars( $contentFromDB ); ?>
+                {!! Form::textarea('text_4', null, ['class' => 'form-control', 'id' => 'description4']) !!}
+            </div><br>
+        </div>
+        <div>
+            <div>
                 {!! Form::label('text_1', 'متن شماره 1:') !!}
                 <? echo htmlspecialchars( $contentFromDB ); ?>
-                {!! Form::textarea('text_1', null, ['class' => 'form-control']) !!}
+                {!! Form::textarea('text_1', null, ['class' => 'form-control', 'id' => 'description']) !!}
             </div><br>
+        </div>
+
+        {{-- path of photo --}}
+        <div class="form-group row">
+            <label for="path" class="col-md-1 col-form-label text-md-right">{{ __('عکس دوره ') }}</label>
+
+            <div class="col-md-11">
+                <input id="path" type="file" class="form-control" @error('path') is-invalid @enderror" name="path"
+                    value="{{ old('path') }}" required autocomplete="path" autofocus>
+
+                @error('path')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        {{-- alt of photo --}}
+        <div class="form-group row">
+            <label for="alt" class="col-md-1 col-form-label text-md-right">{{ __('عکس alt') }}</label>
+
+            <div class="col-md-11">
+                <input id="path" type="text" class="form-control" @error('alt') is-invalid @enderror" name="alt"
+                    value="{{ old('alt') }}" required autocomplete="alt" autofocus>
+
+                @error('alt')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        {{-- name of photo --}}
+        <div class="form-group row">
+            <label for="img_name" class="col-md-1 col-form-label text-md-right">{{ __('عکس name') }}</label>
+
+            <div class="col-md-11">
+                <input id="path" type="text" class="form-control" @error('img_name') is-invalid @enderror" name="img_name"
+                    value="{{ old('img_name') }}" required autocomplete="img_name" autofocus>
+
+                @error('img_name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
         </div>
         <div>
             {!! Form::submit('ذخیره', ['class' => 'btn btn-primary']) !!}
@@ -68,7 +137,15 @@
     <script>
         CKEDITOR.replace('description', {
             language: 'fa',
-
+        });
+        CKEDITOR.replace('description2', {
+            language: 'fa',
+        });
+        CKEDITOR.replace('description3', {
+            language: 'fa',
+        });
+        CKEDITOR.replace('description4', {
+            language: 'fa',
         });
     </script>
 @endsection
