e9b6b6a47323f6e373d00f801d60f3ff68bf262c
danial <danialdark12098@gmail.com>
2021-12-20 23:39:03 +0330

    adding mmc for comments

---
+++ database/migrations/2021_12_20_200201_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("writer_id")->constrained("users")->onDelete("cascade")->onUpdate("cascade");
+            $table->foreignId("parent_id")->nullable()->constrained("comments")->onDelete("cascade")->onUpdate("cascade");
+            $table->text("text");
+            $table->tinyInteger("status")->default(0)->comment("0 is not accept and 1 is accept");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
