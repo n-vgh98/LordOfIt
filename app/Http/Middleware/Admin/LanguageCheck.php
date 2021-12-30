2465b00c1fad92521a82dcb5d7d3ad9adeaf0b8b
Danial <danialdark12098@gmail.com>
2021-12-30 13:23:45 +0330

    making multi langual fixes

--- app/Http/Middleware/Admin/LanguageCheck.php
+++ app/Http/Middleware/Admin/LanguageCheck.php
@@ -19,33 +19,25 @@ class LanguageCheck
      */
     public function handle(Request $request, Closure $next)
     {
-
         $x = substr($request->getPathInfo(), 1, 2);
-        $ip = $request->ip();
-        $geoip = new GeoIPLocation();
-        $geoip->setIP($ip);
-        $country = $geoip->getCountry();
-        if ($country == "iran") {
-            URL::defaults(['locale' => "fa"]);
-            App::setLocale("fa");
-            if ($x == "fa") {
-                return $next($request);
-            } else {
-                App::setLocale("en");
-                return $next($request);
-
-            }
-
-        } else {
-            URL::defaults(['locale' => "en"]);
-            App::setLocale("en");
-            if ($x == "en") {
-                return $next($request);
-            } else {
+        if ($x == "" or $x == null) {
+            $ip = $request->ip();
+            $geoip = new GeoIPLocation();
+            $geoip->setIP($ip);
+            $country = $geoip->getCountry();
+            if ($country == "iran") {
+                URL::defaults(['locale' => "fa"]);
                 App::setLocale("fa");
                 return $next($request);
-
+            } else {
+                URL::defaults(['locale' => "en"]);
+                App::setLocale("en");
+                return $next($request);
             }
+        } else {
+            URL::defaults(['locale' => $x]);
+            App::setLocale($x);
+            return $next($request);
         }
     }
 }
